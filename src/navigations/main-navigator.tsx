08c0ad7e5f87b3080af28323aec6d2e173554510
hkhieudev <hk_hieu@lermo.co.jp>
2023-10-01 19:50:42 +0700

    :sparkles: update network

--- src/navigations/main-navigator.tsx
+++ src/navigations/main-navigator.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '@pages/welcome';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 type IMainNavigator = {
   welcome: undefined;
@@ -8,6 +9,23 @@ type IMainNavigator = {
 
 const Stack = createStackNavigator<IMainNavigator>();
 export function MainNavigator() {
+  const netInfo = useNetInfo({
+    reachabilityUrl: process.env.BASE_API,
+    reachabilityTest: async (response) => response.status === 204,
+    reachabilityLongTimeout: 60 * 1000, // 60s
+    reachabilityShortTimeout: 5 * 1000, // 5s
+    reachabilityRequestTimeout: 15 * 1000, // 15s
+    reachabilityShouldRun: () => true,
+    shouldFetchWiFiSSID: true,
+    useNativeReachability: false,
+  });
+
+  useEffect(() => {
+    return () => {
+      console.log(netInfo);
+    };
+  }, [netInfo]);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="welcome" component={Welcome} />
